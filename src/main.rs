12ed2336edaad2cd559c8507ea43ac49a771f3e8
Soverclysm <spam.reply55@gmail.com>
2022-01-15 00:29:47 +0000

    Added UI template code
    Also changed some names

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 
-use egui::Ui;
+mod app;
 
 fn main() {
     println!("Hello world!");
+
+    let app = app::DndthingApp::default();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
+
 }
